64f25b67a53ee29c13db739d7f5a87659874dca4
Navneet <navneetbarmase00@gmail.com>
2025-12-13 17:32:39 +0530

    auth route fixes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,7 +29,7 @@ const regex = /^(?=.*[A-Za-z])[A-Za-z0-9._]+$/;
 
 if (!regex.test(username)) {
   return res.status(400).json({
-    error: "Invalid username format",
+    message: "Invalid username format",
   });
 }
     // Check if user exists
